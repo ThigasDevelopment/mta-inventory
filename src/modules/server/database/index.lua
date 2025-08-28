b7850049ed058e6689d38a3b7ab0d1bfb42e5e23
ThigasDevelopment <maia.thico@gmail.com>
2025-08-27 22:49:49 -0300

    :sparkles: Added `load` method on `database` module.

--- src/modules/server/database/index.lua
+++ src/modules/server/database/index.lua
@@ -38,6 +38,60 @@ function Database:connect ()
 	return true;
 end
 
+function Database:load ()
+	if (not isElement (self.connection)) then
+		return false;
+	end
+
+	-- load table's schema.
+	dbExec (self.connection, [[
+		CREATE TABLE IF NOT EXISTS `items` (
+			`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+			`owner` TEXT NOT NULL,
+
+			`item` TEXT NOT NULL,
+			`slot` TEXT NOT NULL DEFAULT "1",
+			`amount` INTEGER NOT NULL DEFAULT 0,
+
+			`data` JSON NOT NULL,
+			`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+	]]);
+
+	if (not self.data['items']) then
+		self.data['items'] = { };
+	end
+
+	-- load data's from schema.
+	dbQuery (
+		function (qh)
+			local result = qh:poll (-1);
+			if (#result < 1) then
+				return false;
+			end
+
+			for _, row in pairs (result) do
+				if (not self.data['items'][row.owner]) then
+					self.data['items'][row.owner] = { };
+				end
+
+				self.data['items'][row.owner][row.slot] = {
+					id = row.id,
+
+					item = row.item,
+					amount = row.amount,
+
+					data = fromJSON (row.data),
+					created_at = row.created_at,
+				};
+			end
+			return true;
+		end, self.connection, 'SELECT * FROM `items`;'
+	);
+
+	return true;
+end
+
 -- event's resource's
 addEventHandler (EVENT_NAME .. ':loaded', resourceRoot,
 	function ()
